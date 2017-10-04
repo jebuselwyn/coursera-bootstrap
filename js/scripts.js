e47e80d5e75f27df75c42ed71f4d6092d67ac80a
Jebuselwyn Martin <jebuselwyn.martin@accenture.com>
2017-10-04 23:55:46 +0530

    NPM Scripts#1

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+<script>
+$(document).ready(function(){
+    $("#mycarousel").carousel({interval: 2000});
+    $("#carousel-button").click(function(){
+        if($("#carousel-button").children("span").hasClass("fa-pause")){
+            $("#mycarousel").carousel('pause');
+            $("#carousel-button").children("span").removeClass("fa-pause");                   
+            $("#carousel-button").children("span").addClass("fa-play");                   
+        }
+        else{
+            $("#mycarousel").carousel('cycle');
+            $("#carousel-button").children("span").removeClass("fa-play");                   
+            $("#carousel-button").children("span").addClass("fa-pause");                   
+        }
+    });
+});
+</script>
