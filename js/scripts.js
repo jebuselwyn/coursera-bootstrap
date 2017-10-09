1eb9f295ec2812ea10b2f1a9403afd7c0418cb04
Jebuselwyn Martin <jebuselwyn.martin@accenture.com>
2017-10-09 07:34:51 +0530

    Assignment #4

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $("#mycarousel").carousel({interval: 2000});
+    $("#carousel-button").click(function(){
+        if($("#carousel-button").children("span").hasClass("fa-pause")){
+            $("#mycarousel").carousel('pause');
+            $("#carousel-button").children("span").removeClass("fa-pause");                   
+            $("#carousel-button").children("span").addClass("fa-play");                   
+        }
+        else{
+            $("#mycarousel").carousel('cycle');
+            $("#carousel-button").children("span").removeClass("fa-play");                   
+            $("#carousel-button").children("span").addClass("fa-pause");                   
+        }
+    });
+    $("#login-button").click(function(){
+        $("#loginModal").modal('toggle')
+    });
+    $("#reserve-button").click(function(){
+        $("#reservationModal").modal('toggle')
+    });
+});
